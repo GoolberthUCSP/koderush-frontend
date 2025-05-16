c482ea1d2b6caf52239258577428784527be9061
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2025-05-16 08:30:39 -0500

    Second commit

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,19 +8,12 @@ function Content(props) {
     const { getSelectedPage } = usePageContext();
 
     return (
-        <div className="row">
-            <aside className="col-md-3 my-3 border-end hiding">
-                {/* Aside content */}
-                This is the sidebar
-            </aside>
-            <main className="col-md-9 py-3">
-                {/* Main content */}
-                {getSelectedPage() === 'home' && <HomePage />}
-                {getSelectedPage() === 'leaderboard' && <LeaderboardPage />}
-                {getSelectedPage() === 'contests' && <ContestsPage />}
-                {getSelectedPage() === 'profile' && <ProfilePage />}
-            </main>
-        </div>
+        <>
+            {getSelectedPage() === 'home' && <HomePage />}
+            {getSelectedPage() === 'leaderboard' && <LeaderboardPage />}
+            {getSelectedPage() === 'contests' && <ContestsPage />}
+            {getSelectedPage() === 'profile' && <ProfilePage />}
+        </>
     );
 }
 
